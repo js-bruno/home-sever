523282f20f2076749754b6cdfa8c82dfa1775638
js-bruno <brunocebrsilva@gmail.com>
2026-08-14 09:51:42 -0300

    fix: add nix-minecaft in flake output

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     nix-minecraft.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, nix-minecraft, ... }:
   let
     system = "x86_64-linux";
 
